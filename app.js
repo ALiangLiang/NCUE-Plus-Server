a2dfc7ea16741bac1833b6ed097e8fc2676d4254
root <root@ubuntu.members.linode.com>
2017-03-07 14:58:25 +0800

    Bug fix

--- app.js
+++ app.js
@@ -5,56 +5,92 @@ const
   client = new auth.OAuth2(CONFIG.client_id, '', ''),
   express = require('express'),
   bodyParser = require('body-parser'),
-  app = express();
+  app = express(),
+  PORT = (!process.argv.find((e) => e === '-d')) ? 3000 : 3001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-app.get('/', function(req, res) {
+app.get('/course', function(req, res) {
   console.log(req.query);
-  Comment.findAll({
-      include: [{
-        model: Course,
-        where: {
-          courseName: req.query.courseName,
-          courseClass: req.query.class
-        }
-      }, {
-        model: User
-      }]
+  Course.findCreateFind({
+      where: {
+        courseName: req.query.courseName,
+        courseClass: req.query.class
+      }
     })
     .then((instances) => {
-      const result = instances.map((e) => e.toJSON());
-      console.log(result);
+      const courseId = instances[0].get('id');
+      return Promise.all([
+        Promise.resolve(courseId),
+        Request.count({
+          where: {
+            courseId: courseId
+          }
+        }),
+        Comment.findAll({
+          raw: true,
+          group: ['`comment`.`id`'],
+          attributes: ['id', 'anonymous', 'content', 'updatedAt'],
+          where: {
+            courseId: courseId
+          },
+          include: [{
+            model: User,
+            attributes: ['name']
+          }, {
+            model: Thumb,
+            attributes: [
+              [sequelize.fn('COUNT', sequelize.col('commentId')), 'thumbNum']
+            ],
+            or: true
+          }]
+        })
+      ]);
+    })
+    .then((results) =>
       res.send({
-        comments: result.map((e) => {
+        courseId: results[0],
+        requestCount: results[1],
+        comments: results[2].map((e) => {
           return {
-            author: (e.anonymous) ? '匿名' : e.user.name,
+            id: e.id,
+            author: (e.anonymous === 1) ? '匿名' : e['user.name'],
             content: e.content,
-            time: e.createdAt
+            time: e.updatedAt,
+            isRequest: true,
+            thumbCount: Number(e['thumbs.thumbNum'])
           };
         })
-      });
-    }, (err) => console.error(err));
+      }), (err) => console.error(err));
 });
 
-app.post('/', function(req, res) {
+function verifyIdentity(token) {
+  if (!token) return Promise.reject();
+  return new Promise((resolve, reject) => {
+    console.log(123)
+    client.verifyIdToken(
+      token,
+      CONFIG.client_id,
+      function(e, login) {
+        if (e)
+          return reject(e);
+        return resolve(login);
+      });
+  });
+}
+
+app.post('/comment', function(req, res) {
   const token = req.body.token;
   console.log(req.body);
-  if (!req.body.courseClass || !req.body.courseName || !req.body.content || req.body.anonymous === undefined || !req.body.token)
-    return res.send({
-      isSuccess: false
-    });
-  client.verifyIdToken(
-    token,
-    CONFIG.client_id,
-    function(e, login) {
-      if (e)
-        return res.send({
-          isSuccess: false
-        });
+  if (!req.body.courseClass || !req.body.courseName || !req.body.content || req.body.anonymous === undefined)
+    return res.status(401).send();
+  else if (!req.body.token)
+    return res.status(403).send();
+  verifyIdentity(token)
+    .then((login) => {
       const payload = login.getPayload();
       console.log(payload);
       return Promise.all([
@@ -90,22 +126,117 @@ app.post('/', function(req, res) {
                 }
               }, {
                 include: [User, Course]
-              })
+              });
             });
         })
-        .then((instance) => {
-          res.send({
-            isSuccess: true,
+        .then((instance) =>
+          res.status(201).send({
+            id: instance.toJSON().id,
             author: (req.body.anonymous) ? '匿名' : payload.name,
             content: req.body.content,
-            time: instance.toJSON().createdAt
+            time: instance.toJSON().createdAt,
+            thumbCount: 0
+          }), (err) => console.error(err));
+
+    }, () => res.status(401).send());
+});
+
+app.post('/thumb', function(req, res) {
+  const token = req.body.token;
+  verifyIdentity(token)
+    .then((login) => {
+      const payload = login.getPayload();
+      return User.findCreateFind({
+          where: {
+            email: payload.email,
+            name: payload.name
+          }
+        })
+        .then((instance) =>
+          Thumb.create({
+            userId: instance[0].get('id'),
+            commentId: req.body.commentId
+          }));
+    }, () => res.status(401).send())
+    .then(() => res.status(201).send(), (e) => {console.error(e);return res.status(403).send()});
+});
+
+app.delete('/thumb/:commentId', function(req, res) {
+  const
+    token = req.body.token,
+    commentId = req.params.commentId;
+  verifyIdentity(token)
+    .then((login) => {
+      const payload = login.getPayload();
+      return User.findCreateFind({
+          where: {
+            email: payload.email,
+            name: payload.name
+          }
+        })
+        .then((instance) => {
+          return Thumb.destroy({
+            where: {
+              commentId: commentId
+            },
+            include: [{
+              model: User,
+              where: {
+                email: payload.email
+              }
+            }]
           });
-        }, (err) => console.error(err));
+        }, () => res.status(401).send());
+    })
+    .then(() => res.status(201).send(), () => res.status(404).send());
+});
+
+app.post('/request', function(req, res) {
+  const token = req.body.token;
+  verifyIdentity(token)
+    .then((login) => {
+      const payload = login.getPayload();
+      return User.findCreateFind({
+          where: {
+            email: payload.email,
+            name: payload.name
+          }
+        })
+        .then((instances) =>
+          Request.create({
+            userId: instances[0].get('id'),
+            courseId: req.body.courseId
+          }));
+    }, () => res.status(401).send())
+    .then(() => res.status(201).send(), () => res.status(403).send());
+});
+
+app.delete('/request/:courseId', function(req, res) {
+  const
+    token = req.body.token,
+    courseId = req.params.courseId;
+  verifyIdentity(token)
+    .then((login) => {
+      const payload = login.getPayload();
+      return User.findCreateFind({
+          where: {
+            email: payload.email,
+            name: payload.name
+          }
+        })
+        .then((instances) =>
+          Request.destroy({
+            where: {
+              userId: instances[0].get('id'),
+              courseId: courseId
+            }
+          }), () => res.status(401).send())
+        .then(() => res.status(201).send(), () => res.status(404).send());
     });
 });
 
-app.listen(3000, function() {
-  console.log('App listening on port 3000!');
+app.listen(PORT, function() {
+  console.log('App listening on port ' + PORT);
 });
 
 const
@@ -162,18 +293,33 @@ const
       unique: true,
       fields: ['courseClass', 'courseName']
     }]
+  }),
+  Thumb = sequelize.define('thumb', {}, {
+    indexes: [{
+      unique: true,
+      fields: ['commentId', 'userId']
+    }]
+  }),
+  Request = sequelize.define('request', {}, {
+    indexes: [{
+      unique: true,
+      fields: ['courseId', 'userId']
+    }]
   });
+
 User.hasMany(Comment);
 Course.hasMany(Comment);
+Comment.hasMany(Thumb);
+User.hasMany(Thumb);
+Course.hasMany(Request);
+User.hasMany(Request);
+
 Comment.belongsTo(User);
 Comment.belongsTo(Course);
+Thumb.belongsTo(Comment);
+Thumb.belongsTo(User);
+Request.belongsTo(Course);
+Request.belongsTo(User);
 
-sequelize.sync({
-    force: true
-  })
-  .then(() => {
-    const fs = require('fs');
-    return sequelize.query(fs.readFileSync('init.sql', 'utf8'))
-      .then(() => sequelize.query(fs.readFileSync('init2.sql', 'utf8')));
-  })
+sequelize.sync()
   .then(function() {}, (err) => console.error(err));
